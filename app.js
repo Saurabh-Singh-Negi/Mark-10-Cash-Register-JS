6f8e23be478dd3197d3555ad0184551386760a00
Saurabh-Singh-Negi <saurabhnegi0007@gmail.com>
2022-04-06 08:21:16 +0530

    console logging billAmount

---
+++ app.js
@@ -0,0 +1,6 @@
+const billAmount = document.querySelector("#bill-amount");
+const btnCheck = document.querySelector("#btn-check");
+const cashGiven = document.querySelector("#cash-given");
+
+// console.log(cashGiven.value);
+console.log(billAmount.value);

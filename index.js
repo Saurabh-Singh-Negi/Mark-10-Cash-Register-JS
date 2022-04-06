0f07d311612121ba16fb1f7b67c4018c46a15a25
Saurabh-Singh-Negi <saurabhnegi0007@gmail.com>
2022-04-06 07:45:28 +0530

    added selector for bill amount in js

---
+++ index.js
@@ -0,0 +1,11 @@
+const billAmount = document.querySelector("#bill-amount");
+// const btnCheck = document.querySelector("#btn-check")
+// const errorMsg = document.querySelector("#error-msg");
+
+// btnCheck.addEventListener("click", function clickHandler() {
+//     if(billAmount<0) {
+//         errorMsg.innerText = "enter positive bill amount";
+//     }
+// })
+
+console.log(billAmount.value);

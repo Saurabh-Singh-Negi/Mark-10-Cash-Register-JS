329961b5eef774e38f3684b091d3cd24ba163a7d
Saurabh-Singh-Negi <saurabhnegi0007@gmail.com>
2022-04-06 16:04:04 +0530

    error validation

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 const billAmount = document.querySelector("#bill-amount");
 const btnCheck = document.querySelector("#btn-check");
 const cashGiven = document.querySelector("#cash-given");
+const errorMsg = document.querySelector("#error-msg");
 
-// console.log(cashGiven.value);
-console.log(billAmount.value);
+btnCheck.addEventListener("click", function validateBillAndCash() {
+    errorMsg.style.display = "none";
+    if(billAmount.value > 0) {
+        if(cashGiven.value >= billAmount.value) {
+
+        }
+        else {
+            printError("cash given is lesser than bill amount");
+        }
+
+    }
+    else {
+        printError("bill should be greater than 0");
+    }
+})
+
+function printError(message) {
+    errorMsg.style.display = "block";
+    errorMsg.innerText = message;
+}
+
